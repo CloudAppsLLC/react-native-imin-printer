e954fa42eed90a30f5c351d8eb39ac69f045ac65
aalbaharnah <baharnah.13@gmail.com>
2023-10-01 08:49:40 +0300

    remove files

--- android/src/main/java/com/iminprinter/utils/FontType.java
+++
@@ -1,7 +0,0 @@
-package com.iminprinter.utils;
-
-/* loaded from: classes20.dex */
-public class FontType {
-    public static int FONT_A = 0;
-    public static int FONT_B = 1;
-}
--- android/src/main/java/com/iminprinter/utils/Inch.java
+++
@@ -1,7 +0,0 @@
-package com.iminprinter.utils;
-
-/* loaded from: classes20.dex */
-public class Inch {
-    public static int PRINT_HEAD_INCH_58 = 48;
-    public static int PRINT_HEAD_INCH_80 = 72;
-}
